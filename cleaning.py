b48fb1311281718c2de1346bb2b8c098b6bde467
jasmpa <82006282+jasmpa-1777547@users.noreply.github.com>
2022-05-30 20:30:39 -0700

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -2,8 +2,10 @@ import pandas as pd
 
 
 def main():
-    data = pd.read_csv('data/netflix_titles.csv')
-    print(data)
+    netflix = pd.read_csv('data/netflix_titles.csv')
+    disney = pd.read_csv('data/disney_plus_titles.csv')
+    amazon = pd.read_csv('data/amazon_prime_titles.csv')
+    print(disney)
 
 
 if __name__ == '__main__':
