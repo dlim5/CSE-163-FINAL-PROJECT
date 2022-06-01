6fdf2586b6742bb1f216e96a8c72f0a7326bdc20
Jason Lim <dlim5@uw.edu>
2022-06-01 13:47:29 -0700

    f

--- cleaning.py
+++ cleaning.py
@@ -2,17 +2,20 @@ import pandas as pd
 
 
 def covid_data(file):
-    data = pd.read_csv(file)
-    data_covid = data[
-        (data["release_year"] >= 2019) & (data["release_year"] <= 2021)]
+    """
+    Filters 2019-2021 year(covid) for both movies and TV shows
+    """
+    data = pd.read_csv(file, index_col="date_added", parse_dates=True)
+    data_covid = data.loc["2019":"2021"]
     return data_covid
 
 
 def non_covid_data(file):
-    data = pd.read_csv(file)
-    data_non_covid = data[(data["release_year"] != 2019) & (
-        data["release_year"] != 2020) & (
-            data["release_year"] != 2021)]
+    """
+    filters non-covid year for both movies and TV shows
+    """
+    data = pd.read_csv(file, index_col="date_added", parse_dates=True)
+    data_non_covid = data.drop(data.loc["2019":"2021"].index)
     return data_non_covid
 
 
--- main.py
+++ main.py
@@ -7,18 +7,34 @@ from cleaning import covid_data, non_covid_data
 
 sns.set()
 
-def top_rating_genres(amazon, disney, netflix):
-    print(amazon)
+def netflix_top_rating_genres(data):
+    netflix_covid = data[data["type"] == "Movie"]
+    max_rating = netflix_covid.groupby("rating")["rating"].count().idxmax()
+    data_highest_rating = netflix_covid[netflix_covid["rating"] == max_rating]
+    genres = data_highest_rating["listed_in"].str.split(",")
+    
+    genre_dictionary = {}
+    for movie in genres:
+        for genre in movie:
+            if genre not in genre_dictionary:
+                genre_dictionary[genre] = 1
+            else:
+                genre_dictionary[genre] += 1
+    keys = genre_dictionary.keys()
+    values = genre_dictionary.values()
+    plt.bar(keys, values)
+    plt.gcf().autofmt_xdate()
+    plt.show()
+
 
 def main():
-    amazon_covid = covid_data("data/amazon_prime_titles.csv")
-    disney_covid = covid_data("data/disney_plus_titles.csv")
+    # amazon_covid = covid_data("data/amazon_prime_titles.csv")
+    # disney_covid = covid_data("data/disney_plus_titles.csv")
     netflix_covid = covid_data("data/netflix_titles.csv")
-    top_rating_genres(amazon_covid, disney_covid, netflix_covid)
+    netflix_top_rating_genres(netflix_covid)
     #sns.catplot(x="rating", kind="count", hue="rating", data=data)
     #plt.xlabel("rating", rotation="vertical")
     #plt.savefig("amazon_rating_plot")
-
 
 
 if __name__ == '__main__':
