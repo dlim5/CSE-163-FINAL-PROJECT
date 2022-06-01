0740959025b7555bb764e75639ceb455fd91f215
Jason Lim <dlim5@uw.edu>
2022-05-31 22:27:50 -0700

    j

--- cleaning.py
+++ cleaning.py
@@ -1,30 +1,23 @@
 import pandas as pd
 
 
+def covid_data(file):
+    data = pd.read_csv(file)
+    data_covid = data[
+        (data["release_year"] >= 2019) & (data["release_year"] <= 2021)]
+    return data_covid
+
+
+def non_covid_data(file):
+    data = pd.read_csv(file)
+    data_non_covid = data[(data["release_year"] != 2019) & (
+        data["release_year"] != 2020) & (
+            data["release_year"] != 2021)]
+    return data_non_covid
+
+
 def main():
-    netflix = pd.read_csv('data/netflix_titles.csv')
-    netflix_covid = netflix[
-        (netflix["release_year"] >= 2019) & (netflix["release_year"] <= 2021)]
-    netflix_non_covid = netflix[
-        (netflix["release_year"] != 2019) & (
-            netflix["release_year"] != 2020) & (
-                netflix["release_year"] != 2021)]
-    disney = pd.read_csv('data/disney_plus_titles.csv')
-    disney_covid = disney[
-        (disney["release_year"] >= 2019) & (disney["release_year"] <= 2021)]
-    disney_non_covid = disney[
-        (disney["release_year"] != 2019) & (
-            disney["release_year"] != 2020) & (
-                disney["release_year"] != 2021)]
-    amazon = pd.read_csv('data/amazon_prime_titles.csv')
-    amazon_covid = amazon[
-        (amazon["release_year"] >= 2019) & (amazon["release_year"] <= 2021)]
-    amazon_non_covid = amazon[
-        (amazon["release_year"] != 2019) & (
-            amazon["release_year"] != 2020) & (
-                amazon["release_year"] != 2021)]
-    
-    print(netflix_non_covid)
+    pass
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # Start of aggregating and evaluating datas
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+from cleaning import covid_data, non_covid_data
+
+
+sns.set()
+
+
+def main():
+    data = covid_data("data/amazon_prime_titles.csv")
+    sns.catplot(x="rating", kind="count", hue="rating", data=data)
+    plt.xlabel("rating", rotation="vertical")
+    plt.savefig("amazon_rating_plot")
+
+
+
+if __name__ == '__main__':
+    main()
