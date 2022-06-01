1425210f827cbf2046d01ac9f5c913da2445f03b
jasmpa <82006282+jasmpa-1777547@users.noreply.github.com>
2022-05-31 22:43:04 -0700

    l

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ from cleaning import covid_data, non_covid_data
 
 sns.set()
 
+def top_rating_genres(amazon, disney, netflix):
+    print(amazon)
 
 def main():
-    data = covid_data("data/amazon_prime_titles.csv")
-    sns.catplot(x="rating", kind="count", hue="rating", data=data)
-    plt.xlabel("rating", rotation="vertical")
-    plt.savefig("amazon_rating_plot")
+    amazon_covid = covid_data("data/amazon_prime_titles.csv")
+    disney_covid = covid_data("data/disney_plus_titles.csv")
+    netflix_covid = covid_data("data/netflix_titles.csv")
+    top_rating_genres(amazon_covid, disney_covid, netflix_covid)
+    #sns.catplot(x="rating", kind="count", hue="rating", data=data)
+    #plt.xlabel("rating", rotation="vertical")
+    #plt.savefig("amazon_rating_plot")
 
 
 
