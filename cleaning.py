dcea0e25535d61334d6c8ed7368950f0a97edb5d
jasmpa <82006282+jasmpa-1777547@users.noreply.github.com>
2022-05-31 23:40:12 -0700

    i

--- cleaning.py
+++ cleaning.py
@@ -4,15 +4,15 @@ import pandas as pd
 def covid_data(file):
     data = pd.read_csv(file)
     data_covid = data[
-        (data["release_year"] >= 2019) & (data["release_year"] <= 2021)]
+        (data["date_added"] >= 2019) & (data["date_added"] <= 2021)]
     return data_covid
 
 
 def non_covid_data(file):
     data = pd.read_csv(file)
-    data_non_covid = data[(data["release_year"] != 2019) & (
-        data["release_year"] != 2020) & (
-            data["release_year"] != 2021)]
+    data_non_covid = data[(data["date_added"] != 2019) & (
+        data["date_added"] != 2020) & (
+            data["date_added"] != 2021)]
     return data_non_covid
 
 
